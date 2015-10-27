29917a97a348ae6f6466a53d8772afc96c3d1a26
Sai Pc <schand31@asu.edu>
2015-10-27 00:40:09 -0700

    fixed up some edge cases, works fine

--- checkServerUp.py
+++ checkServerUp.py
@@ -6,18 +6,22 @@ def makeRequest(url):
         r = requests.get(url, verify=False, timeout=5)
         return r
     except Exception as e:
+        print "Timeout"
         return None
 
 
 def checkIfUp():
     serversUp = []
-    for x in xrange(1, 15):
+    for x in xrange(1, 17):
         url = 'https://group' + str(x) + '.mobicloud.asu.edu'
         print url
         r = makeRequest(url)
         if r and r.status_code == 200 and r.headers != None and len(r.text) > 100:
             print "Server is up"
+            serversUp.append(x)
         else:
             print "Still down"
+    for server in serversUp:
+        print "group" + str(server) + " is up"
 
 checkIfUp()
