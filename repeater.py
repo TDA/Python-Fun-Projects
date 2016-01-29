c8d34137e76b14e83bdecf3647f8e95b75c06d05
Sai Pc <schand31@asu.edu>
2016-01-29 15:58:29 -0700

    funny repeating

--- repeater.py
+++ repeater.py
@@ -3,4 +3,8 @@ __author__ = 'saipc'
 def repeat(word, times):
     return word * times
 
-print repeat("possible", 999999)
+#print repeat("possible", 999999)
+for x in xrange(0,999999):
+    print "Devanshi says: ", repeat("plausible", 2)
+    print "Shruti says: ", repeat("possible", 2)
+    print "But Sai allegedly says: ", repeat("pozzible", 3)
