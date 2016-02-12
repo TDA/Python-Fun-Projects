42a4eda48338c133ed7a30df485681a48917bc34
Sai Pc <schand31@asu.edu>
2016-02-12 00:41:08 -0700

    started rhyming words

---
+++ RhymingWordFinder.py
@@ -0,0 +1,23 @@
+__author__ = 'saipc'
+import requests
+from bs4 import BeautifulSoup
+# http://www.rhymezone.com/r/rhyme.cgi?typeofrhyme=perfect&Word=word&loc=spellmap3
+
+def find_words(word):
+    words = []
+    headers = {
+        'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Host' : 'www.rhymezone.com',
+        'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36'
+    }
+    r = requests.get("http://www.rhymezone.com/r/rhyme.cgi?typeofrhyme=perfect&Word=" + word, headers=headers)
+
+    print(r.status_code)
+    print(r.text)
+    html = BeautifulSoup(r.text, 'html.parser')
+    # print(html)
+
+    return words
+
+find_words("sai")
