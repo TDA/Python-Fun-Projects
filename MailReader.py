b1c430b858eb0d00408542210a91fda8fb19521f
Sai Pc <schand31@asu.edu>
2016-02-16 00:48:30 -0700

    mail reader added

---
+++ MailReader.py
@@ -0,0 +1,15 @@
+__author__ = 'saipc'
+
+import sys
+import mailbox
+
+def read_mails(filename):
+    mbox = mailbox.mbox(filename)
+    for message in mbox:
+       print message
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print 'Usage: python Mailreader mailfilename'
+        sys.exit(1)
+    read_mails(sys.argv[1])
