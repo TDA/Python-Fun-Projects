95a9ed71bf80abf7f12f3e0db5513edacb7d15cd
Sai Chandramouli <schand31@asu.edu>
2016-07-03 09:20:36 -0700

    added return wishes

--- happybday.py
+++ happybday.py
@@ -9,11 +9,13 @@ print(wish('Sai'))
 def thank(name):
     thank_yous = ['Thank you so much', 'Thank you very much', 'Thank you!!']
     smileys = [':)', ':D', ';)']
-    return_wishes = ['Hope you are doing well!', 'What are you upto?', 'Hope you are doing great :)', 'Take care!']
+    return_wishes = ['Hope you are doing well!', 'What are you upto?', 'Hope you are doing great', 'Take care!']
 
     return random.choice(thank_yous) + \
            ' @' + name + ' ' + \
-           (random.choice(smileys) + random.choice(smileys))
+           (random.choice(smileys) + random.choice(smileys)) + ' ' + \
+           random.choice(return_wishes) + ' ' + \
+           random.choice(smileys)
 
 for i in xrange(5):
     print(thank('sai'))
